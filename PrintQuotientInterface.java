1e3030ce4eb927ce634edd83709dd6ed2b48f653
AyanPersonal <ayanjdeka@gmail.com>
2019-06-11 16:28:45 -0400

    Partner B added what he learned

--- PrintQuotientInterface.java
+++
@@ -1,7 +0,0 @@
-
-//Ayan testing Comments 
-public interface PrintQuotientInterface{
-	//hello
-	public void printQuotient(float a, float b);
-}
-
--- Runner.java
+++ Runner.java
@@ -1,6 +1,8 @@
 public class Runner{
 	public static void main(String[] args) {
 
+		//Ronit Learned that lamdafunctions are used to make a method more effecient and write a method in less code
+
 		PrintQuotientInterface printQuotient = (float a, float b) -> System.out.printf("%.3f / %.3f == %.3f",a,b,a/b);
 		printQuotient.printQuotient(1,3);
 
