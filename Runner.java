fc1403a98fc3c92a1b820e5d90892bdf2fc99518
AyanPersonal <ayanjdeka@gmail.com>
2019-06-11 16:36:18 -0400

    Merge branch 'CommentsPartnerB'

--- Runner.java
+++ Runner.java
@@ -1,8 +1,9 @@
 public class Runner{
 	public static void main(String[] args) {
 
-		//Ayan Learned that Gitkraken is a UserInterface to store and to share code with others
 
+		//Ronit Learned that lamdafunctions are used to make a method more effecient and write a method in less code
+		//Ayan Learned that Gitkraken is a UserInterface to store and to share code with others
 		PrintQuotientInterface printQuotient = (float a, float b) -> System.out.printf("%.3f / %.3f == %.3f",a,b,a/b);
 		printQuotient.printQuotient(1,3);
 
