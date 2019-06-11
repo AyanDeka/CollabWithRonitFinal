9ba212879d8097ca6ab7ee57ebd4be7ee8565b42
AyanPersonal <ayanjdeka@gmail.com>
2019-06-11 16:24:03 -0400

    Combined All Work to One Runner

--- Runner.java
+++ Runner.java
@@ -1,15 +1,11 @@
 public class Runner{
-
-<<<<<<< HEAD
 	public static void main(String[] args) {
 
 		PrintQuotientInterface printQuotient = (float a, float b) -> System.out.printf("%.3f / %.3f == %.3f",a,b,a/b);
 		printQuotient.printQuotient(1,3);
 
-=======
-	public static void main(String[]args){
 		PerfectSquareInterface squareInterface = (int num) -> Math.sqrt(num) == Math.round(Math.sqrt(num));
 		System.out.println(squareInterface.perfectSum(49));
->>>>>>> Perfect_Square
+
 	}
 }
